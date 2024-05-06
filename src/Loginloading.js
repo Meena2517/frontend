7aa4124a21060391205a8aaa5f3c354e5ab1b32a
Revanth <revanthpatnani@gmail.com>
2024-05-06 22:41:59 +0530

    Loading added

---
+++ src/Loginloading.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Loginloading = () => {
+  return (
+    <div>
+      <div className='loader-container'><div id="page">
+                <div id="container">
+                    <div id="ring"></div>
+                    <div id="ring"></div>
+                    <div id="ring"></div>
+                    <div id="ring"></div>
+                    <div id="h3">Loading</div>
+                </div>
+            </div></div>
+    </div>
+  )
+}
+
+export default Loginloading
